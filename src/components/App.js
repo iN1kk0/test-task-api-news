8a64a6aba781d3097c57e980d3e5f6e4ce767d75
iN1kk0 <nikolay17@ukr.net>
2021-08-03 03:37:55 +0300

    Sorting

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,60 @@ import "../styles/App.css";
 const App = () => {
   const [news, setNews] = useState([]);
 
-  useEffect(() =>
+  useEffect(() => {
     fetch("https://api.hnpwa.com/v0/news/1.json")
       .then((res) => res.json())
-      .then((res) => setNews(res))
-  ),
-    [news];
+      .then((res) => setNews(res));
+  }, []);
+
+  const compareValues = (key, order = "asc") => {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        return 0;
+      }
+
+      const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+      const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return order === "desc" ? comparison * -1 : comparison;
+    };
+  };
+
+  const sortColumn = (key) => {
+    const sortedNews = [...news];
+    const attrsort = event.target.getAttribute("data-sort");
+
+    sortedNews.sort(compareValues(key, attrsort));
+    setNews(sortedNews);
+
+    if (attrsort === "asc") {
+      event.target.setAttribute("data-sort", "desc");
+    } else {
+      event.target.setAttribute("data-sort", "asc");
+    }
+  };
 
   return (
     <>
       <h1>News</h1>
-      <table className="m-table">
+      <table>
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Time</th>
-            <th>Domain</th>
+            <th data-sort="asc" onClick={() => sortColumn("title")}>
+              Title
+            </th>
+            <th data-sort="asc" onClick={() => sortColumn("time")}>
+              Time
+            </th>
+            <th data-sort="asc" onClick={() => sortColumn("domain")}>
+              Domain
+            </th>
           </tr>
         </thead>
         <tbody>
